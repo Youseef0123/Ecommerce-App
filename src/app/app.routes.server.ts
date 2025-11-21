e75cc1163712ea0fec06fb7b77772186a5a4bc56
Youssef-3131 <Youssef.Mahmoud@intcore.com>
2025-11-21 22:06:27 +0200

    last update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,38 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'products',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'brands',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'categories',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'allorders',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
